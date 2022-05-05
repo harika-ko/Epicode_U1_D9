a38911232d6782714468bfab132d7bf357ca1818
Harika_Kommuri <harikakommuri35@gmail.com>
2022-05-05 14:08:52 +0200

    Solved Exercise 3

--- homeWork.js
+++ homeWork.js
@@ -38,7 +38,15 @@ crazySum(5, 5)
  It should return triple their absolute difference if the given number is greater than 19.
 */
 
-/* WRITE YOUR ANSWER HERE */
+function crazyDiff(num) {
+    if (num > 19) {
+        console.log((num - 19) * 3)
+    } else {
+        console.log(19 - num)
+    }
+}
+
+crazyDiff(22)
 
 /* EXERCISE 4
  Write a function called "boundary" which accept an integer parameter n and returns true if n is within 20 and 100 (included) or if n it's equal to 400.
